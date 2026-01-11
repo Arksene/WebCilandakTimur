80603c4bf6438c6be615fa36a558c1d0db7ad26f
Arksene <150993150+Arksene@users.noreply.github.com>
2026-01-12 02:26:19 +0700

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -28,3 +28,7 @@ app.use("/api/chat", chatRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("API OK");
+});
