525ed5876ffca0b7285830a074592e4ce0f4d2f6
Muhammad Eka <m.eka.dwi.pratama@gmail.com>
2026-01-12 15:05:39 +0700

    Fix Form berita

--- Backend/src/lib/nodeMailer.js
+++ Backend/src/lib/nodeMailer.js
@@ -4,18 +4,10 @@ import "dotenv/config";
 export async function sendEmail(to, subject, text) {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_SENDER,
         pass: process.env.EMAIL_PASSWORD,
-      },
-      connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
-      tls: {
-        rejectUnauthorized: false,
       },
     });
 
