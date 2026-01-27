7081d0bc951f000c4e69ca928dc3c7e0af8c2d72
Arksene <150993150+Arksene@users.noreply.github.com>
2026-01-27 08:15:11 +0700

    Update emailService.js

--- Backend/src/lib/emailService.js
+++ Backend/src/lib/emailService.js
@@ -3,6 +3,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail(to, subject, text) {
+  console.log("Mengirim email ke:", to);
   try {
     const response = await resend.emails.send({
       from: "Kelurahan Cilandak Timur <onboarding@resend.dev>",
